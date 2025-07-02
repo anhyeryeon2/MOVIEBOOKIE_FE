7f1e31921de205a8f752143e5f981684fd262fe1
anhyeryeon2 <ahr020532@gmail.com>
2025-07-02 08:36:59 +0000

    fix: 빌드오류 수정 (#232)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 "use client";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 import "@/styles/globals.css";
 import { pretendard } from "../app/fonts/pretendard";
 import InAppRedirect from "./_components/inapp-redirect";
 import { ReactQueryProvider } from "./providers/react-query-provider";
-import GlobalLoading from "./_components/global-loading";
 import ToastRenderer from "./_components/toast-renderer";
 import Script from "next/script";
-import DebugLogger from "./_components/debug-logger";
 import { ToastProvider } from "./_context/toast-context";
 import Toast from "./_components/noti-toast";
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -25,9 +23,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <link rel="icon" href="/images/favicon/48x48.png" />
       </head>
       <body>
-        {" "}
         <ToastProvider>
-          {/* <DebugLogger /> */}
           <Script
             src="https://developers.kakao.com/sdk/js/kakao.js"
             strategy="beforeInteractive"
@@ -35,12 +31,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <InAppRedirect />
           <ToastRenderer />
           <ReactQueryProvider>
-            {/* <GlobalLoading /> */}
             <ServiceWorkerDebug />
             {children}
             <SpeedInsights />
             <Toast />
-          </ReactQueryProvider>{" "}
+          </ReactQueryProvider>
         </ToastProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Home from "./(tabs)/home/page";
 import TabsLayout from "./(tabs)/layout";
-import FCMHandler from "./_components/FCM/fcm-handler";
 
 export default function WrappedTabsHome() {
   return (
