ec92b0667e78e2c2c3f2b775dbf8f6a399f7b022
anhyeryeon2 <ahr020532@gmail.com>
2025-07-22 15:02:44 +0000

    refactor: pwa 디버깅UI 추가 (#264)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       </head>
       <body>
         <ToastProvider>
-          {/* <DebugLogger /> */}
+          <DebugLogger />
           <Script
             src="https://developers.kakao.com/sdk/js/kakao.js"
             strategy="beforeInteractive"
